8f67ac2369f404694035732dfe30951106e36031
Rohit Thakur <99536715+BrickRT@users.noreply.github.com>
2023-09-21 18:11:25 +0530

    Update SQL_data_exploration.sql

--- SQL_data_exploration.sql
+++ SQL_data_exploration.sql
@@ -1,3 +1,10 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 select * 
 from Portfolio_project..CovidDeaths 
 order by 3,4
